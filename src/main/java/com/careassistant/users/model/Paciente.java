2d945b98b7636bcf5a9790f6092bf6baad4c76d6
quinto.dev <fhqa3d@gmail.com>
2025-05-10 01:42:13 -0500

    feat: se añaden campos nuevos a pacientes

--- src/main/java/com/careassistant/users/model/Paciente.java
+++ src/main/java/com/careassistant/users/model/Paciente.java
@@ -10,17 +10,23 @@ public class Paciente extends Usuario {
 
 	private Integer edad;
 
+	private String parentesco;
+
 	@NotBlank(message = "La ciudad es obligatoria")
 	private String ciudad;
+
+	private Integer celular;
 
 	public Paciente() {
 		super();
 	}
 
-	public Paciente(UUID id, String nombre, String apellido, String correo, String contraseña, String rol, Integer edad, String ciudad) {
+	public Paciente(UUID id, String nombre, String apellido, String correo, String contraseña, String rol, Integer edad,
+			String parentesco, String ciudad, Integer celular) {
 		super(id, nombre, apellido, correo, contraseña, rol);
 		this.edad = edad;
 		this.ciudad = ciudad;
+		this.celular = celular;
 	}
 
 	public Integer getEdad() {
@@ -31,12 +37,28 @@ public class Paciente extends Usuario {
 		this.edad = edad;
 	}
 
+	public String getParentesco() {
+		return parentesco;
+	}
+
+	public void setParentesco(String parentesco) {
+		this.parentesco = parentesco;
+	}
+
 	public String getCiudad() {
 		return ciudad;
 	}
 
 	public void setCiudad(String ciudad) {
 		this.ciudad = ciudad;
+	}
+
+	public Integer getCelular() {
+		return celular;
+	}
+
+	public void setCelular(Integer celular) {
+		this.celular = celular;
 	}
 
 }
